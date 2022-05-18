a4aa82eee85767ac8c59f1a303df70816fcbb339
joeytang54 <joeytang54@gmail.com>
2022-05-18 02:03:58 -0400

    The feature Sun has been added Successfully

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,13 @@ public class Main {
         FactorX.Sun="1";
         FactorX.Moon="4";
         FactorX.Planet="2";
-        FactorX.Stars="10000"; 
+        FactorX.Stars="10000";
+
+        //adding features to our sun
+        FeatureSun Xfact=new FeatureSun();
+        Xfact.color="green";
+        Xfact.radius="10000000000km";
+        Xfact.heat="3044428493000kj";
 
     }
 }
